a22d8d6580af063374831aa1dbfd2af7176e4026
mhassan654 <hassansaava@gmail.com>
2022-02-14 17:06:28 +0300

    added more stying on sidebaar

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="bg-white w-80 border-r p-8 -my-2">
-    <sidebar-item route="dashboard.home">
+    <!-- <div class="bg-white w-80 border-r p-8 -my-2"> -->
+    <!-- <sidebar-item route="dashboard.home">
       Home
     </sidebar-item>
     <sidebar-item route="dashboard.components">
       Components
-    </sidebar-item>
+    </sidebar-item> -->
 
     <aside class="w-64" aria-label="Sidebar">
-      <div class="px-3 py-4 overflow-y-auto rounded bg-gray-50 dark:bg-gray-800">
-        <ul class="space-y-2">
+      <div class="px-3 py-4 overflow-y-auto rounded dark:bg-gray-800">
+        <div class="space-y-2">
+          <sidebar-item route="dashboard.home">
+            Home
+          </sidebar-item>
+          <sidebar-item route="dashboard.components">
+            Components
+          </sidebar-item>
+        </div>
+        <!-- <ul class="space-y-2">
           <li>
             <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
               <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" /><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" /></svg>
               <span class="ml-3">Dashboard</span>
             </a>
           </li>
-          <li>
+          <!-- <li>
             <button type="button" class="flex items-center w-full p-2 text-base font-normal text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
               <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd" /></svg>
               <span class="flex-1 ml-3 text-left whitespace-nowrap" sidebar-toggle-item>E-commerce</span>
@@ -71,8 +80,8 @@
               <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd" /></svg>
               <span class="flex-1 ml-3 whitespace-nowrap">Sign Up</span>
             </a>
-          </li>
-        </ul>
+          </li> -->
+        <!-- </ul>  -->
       </div>
     </aside>
   </div>
--- resources/js/components/SidebarItem.vue
+++ resources/js/components/SidebarItem.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="rounded px-6 py-3 my-2 cursor-pointer transition-all duration-100 ease-in-out group"
+  <!-- <div class="rounded px-6 py-3 my-2 cursor-pointer transition-all duration-100 ease-in-out group"
        :class="isCurrentRoute ? 'bg-teal-500' : 'bg-transparent hover:bg-gray-100'"
        @click="go"
   >
     <span class="font-semibold transition-all duration-100 ease-in-out" :class="isCurrentRoute ? 'text-white' : 'text-gray-600 group-hover:text-gray-800'">
       <slot />
     </span>
+  </div> -->
+
+  <div class="flex items-center p-2 text-base cursor-pointer font-normal text-white rounded-lg dark:text-white
+   hover:bg-gray-400 dark:hover:bg-gray-700"
+       :class="isCurrentRoute ? 'bg-teal-500' : 'bg-transparent hover:bg-gray-100'"
+       @click="go"
+  >
+    <svg class="w-6 h-6 text-white transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+         fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"
+    ><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" /><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" /></svg>
+
+    <span class="ml-3" :class="isCurrentRoute ? 'text-white' : 'text-gray-600 group-hover:text-gray-800'">
+      <slot />
+    </span>
+    <!-- </a> -->
   </div>
 </template>
 
